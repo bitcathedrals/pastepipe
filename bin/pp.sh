4355ddc37a6504ce8aab8c1af99ce27229dc0885
Michael Mattie <codermattie@runbox.com>
2024-06-21 13:01:22 -0700

    (feat): add a launcher script

---
+++ bin/pp.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+VENV="tools"
+
+TOOLS=$HOME/tools
+PYENV_ROOT="$TOOLS/pyenv"
+PATH="$TOOLS/local/bin:$PATH"
+PATH="$PYENV_ROOT/bin:$PATH"
+PATH="$PYENV_ROOT/libexec:$PATH"
+
+export PYENV_ROOT PATH
+
+eval "$(pyenv init -)"
+
+
+pyenv activate $VENV
+
+exec pyenv exec pastepipe $@
+
